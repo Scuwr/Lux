ec47e1e1f7e49e59ffb86e9ba60051335c698040
Scuwr <cmarnold@mit.edu>
2023-01-23 22:01:58 -0500

    added simple tools for adding more stories to graph tool

---
+++ ASRS_Picker/datapicker.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+os.chdir('./ASRS_Picker')
+
+selected = pd.read_csv('ACN_Picked.csv')
+
+txt = 'let arr = []\n'
+
+for i in range(20,40):
+    txt += 'arr = arr.concat(`'
+    txt += '\nID: ' + str(selected.iloc[i]['ACN']) + '\n\n';
+    txt += str(selected.iloc[i]['Report']) + '\n`)\n\n'
+
+
+txt += 'for (s of arr){\n'
+txt += '\tlet k = await app.mainService.storyAdd(s).toPromise()\n'
+txt += '\tconsole.log(k)\n'
+txt += '}'
+
+print(txt)
+with open('storyArray.js', 'w') as f:
+    f.write(txt)
