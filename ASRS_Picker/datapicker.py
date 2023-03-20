fae035d7a99e7f31182d2b8096bbad3631d79233
Christian Arnold <cmarnold@mit.edu>
2023-03-20 18:35:43 -0400

    added stories

--- ASRS_Picker/datapicker.py
+++ ASRS_Picker/datapicker.py
@@ -7,7 +7,7 @@ selected = pd.read_csv('ACN_Picked.csv')
 
 txt = 'let arr = []\n'
 
-for i in range(300,320):
+for i in range(340,360):
     txt += 'arr = arr.concat(`'
     txt += '\nID: ' + str(selected.iloc[i]['ACN']) + '\n\n';
     txt += str(selected.iloc[i]['Report']) + '\n`)\n\n'
